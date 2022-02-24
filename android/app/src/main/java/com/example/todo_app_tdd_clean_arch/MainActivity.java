ca94d6ccbfef52d3e5c3a33a5b16e46cb4b05bc5
AhmedAbogameel <gamilco81@gmail.com>
2022-02-24 14:57:29 +0200

    - Initial Packages - Project Structure - Entity

---
+++ android/app/src/main/java/com/example/todo_app_tdd_clean_arch/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.todo_app_tdd_clean_arch;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/features/home/domain/entity/todo.dart
@@ -0,0 +1,8 @@
+class Todo {
+
+  final DateTime dateTime;
+  final String content;
+
+  const Todo({required this.dateTime, required this.content});
+
+}
